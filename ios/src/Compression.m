542de1e43bf54a9e7d0f215ab8d473e690486b65
Francisco Cruz <francisco.cruz@safecloudtech.com>
2021-11-17 12:32:39 +0000

    Modify lib to make the image size coherent, where is enough to provide the width wether in landscape or portrait

--- ios/src/Compression.m
+++ ios/src/Compression.m
@@ -43,6 +43,26 @@
     
     int newWidth = 0;
     int newHeight = 0;
+
+    float ratioBitmap = oldWidth / oldHeight;
+
+     if (ratioBitmap > 1) {    
+         // LANDSCAPE
+         newHeight = maxWidth;
+         newWidth = (int) ( oldWidth * ( maxWidth / oldHeight));
+     } else {
+         // PORTRAIT
+         newWidth = maxWidth;
+         newHeight = (int) ( oldHeight * ( maxWidth / oldWidth));
+     }
+
+     // if (maxWidth < maxHeight) {
+     //     newWidth = maxWidth;
+     //     newHeight = (oldHeight / oldWidth) * newWidth;
+     // } else {
+     //     newHeight = maxHeight;
+     //     newWidth = (oldWidth / oldHeight) * newHeight;
+     // }
     
     if (maxWidth < maxHeight) {
         newWidth = maxWidth;
